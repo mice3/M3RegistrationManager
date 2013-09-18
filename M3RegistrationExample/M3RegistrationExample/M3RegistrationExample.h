004fc796a86b874545b26b1db306b79fa0f43726
rok črešnik <rok.cresnik@gmail.com>
2013-09-18 12:53:17 +0200

    M3RegistrationConstants

---
+++ M3RegistrationExample/M3RegistrationExample/M3RegistrationExample.h
@@ -0,0 +1,22 @@
+// Server address
+#define kServerURL @"http://m3registrationexample.herokuapp.com"
+//#define kServerURL @"http://localhost:1337"
+// Script addresses
+#define kServerCreateDevice @"/createDevice"
+#define kGetUsers @"/showUsers"
+#define kServerChangeEmail @"user/changeEmail"
+#define kServerLogin @"user/login"
+#define kServerForgotPassword @"user/forgotPassword"
+
+// other constants
+#define kFacebookConnected @"facebookConnected"
+#define kUserDeviceId @"userDeviceId"
+#define kSecureCode @"secureCode"
+#define kDeviceActivated @"isActivated"
+
+// Twitter consumer key and secret
+#define kTWConsumerKey @"LaCI4JSXV1XkngJgc0A7A"
+#define kTWConsumerSecret @"WIIye2uxaJUnIhSqZbw16oeI9O68YZubfawT3Np2RnY"
+
+// Facebook state change notification
+#define kFBSessionStateChangedNotification @"it.mice3.m3registrationexample:FBSessionStateChangedNotification"
